d95233743d5d5666893516af6df25ee5003370c2
Hartaithan <hartaithan@gmail.com>
2023-08-09 17:36:15 +0900

    install shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Home: FC = () => {
   return (
-    <main className="flex items-center justify-center p-24">
+    <main className="flex flex-col items-center justify-center p-24">
       <h2 className="text-2xl font-bold">Hello World!</h2>
     </main>
   );
