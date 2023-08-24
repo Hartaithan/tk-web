72ecc4324db2279503740261e49b4a4014e08118
Hartaithan <hartaithan@gmail.com>
2023-08-24 20:30:10 +0900

    add main page header

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 import { getCards } from "@/actions/cards";
 import { FC } from "react";
+import MainHeader from "../components/main/header";
 
 const HomePage: FC = async () => {
   const cards = await getCards();
 
   return (
-    <main className="flex flex-col items-center justify-center p-24">
-      <h2 className="text-2xl font-bold mb-6">Hello World!</h2>
-      {cards.status === 200 ? (
-        <pre className="text-xs overflow-auto max-h-96">
-          {JSON.stringify(cards, null, 2)}
-        </pre>
-      ) : (
-        <h2 className="text-2xl font-bold">Что-то пошло не так :(</h2>
-      )}
+    <main className="flex flex-col">
+      <MainHeader />
+      <div
+        id="content"
+        className="flex flex-1 flex-col items-center justify-center p-24">
+        <h2 className="text-2xl font-bold mb-6">Hello World!</h2>
+        {cards.status === 200 ? (
+          <pre className="text-xs overflow-auto max-h-96">
+            {JSON.stringify(cards, null, 2)}
+          </pre>
+        ) : (
+          <h2 className="text-2xl font-bold">Что-то пошло не так :(</h2>
+        )}
+      </div>
     </main>
   );
 };
---
+++ components/main/header.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const MainHeader: FC = () => {
+  return (
+    <header className="flex items-center justify-center min-h-[48px]">
+      <p className="uppercase font-bold">Карты</p>
+    </header>
+  );
+};
+
+export default MainHeader;
